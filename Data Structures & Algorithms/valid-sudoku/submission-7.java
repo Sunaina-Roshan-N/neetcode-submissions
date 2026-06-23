fc44700716636c0399ef6f7654863633d3552968
Sunaina-Roshan-N <sunaina.roshann.code@gmail.com>
2026-06-23 19:23:16 +0530

    Add: valid-sudoku - submission-7

---
+++ Data Structures & Algorithms/valid-sudoku/submission-7.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        HashSet<String> seen = new HashSet<>();
+
+        for(int i = 0; i < 9; i++) {
+            for(int j = 0; j < 9; j++) {
+                if(board[i][j] != '.') {
+                    if(!seen.add(board[i][j] + "row" + i) ||
+                        !seen.add(board[i][j] + "column" + j) ||
+                        !seen.add(board[i][j] + "row" + i/3 + "column" + j/3))
+                        return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
